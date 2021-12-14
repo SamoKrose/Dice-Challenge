00e5d47ea04c1e169e067c4a3b821b6979867d99
Sami Kaddoura <74143474+SamoKrose@users.noreply.github.com>
2021-12-14 22:09:17 +0000

    Add files via upload

---
+++ dicee.js
@@ -0,0 +1,29 @@
+var randomNumber1 = Math.floor(Math.random() * 6) +1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice 1.png to dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+// if player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "😍🚩Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "🚩player 2 wins!😍"
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw";
+}
